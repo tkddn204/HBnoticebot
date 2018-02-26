00218815d3779341fd555f463be0ce3136e69ba1
SsangWoo <tkddn204@gmail.com>
2018-02-26 12:08:01 +0900

    sa사업단 없앰

--- crawling.py
+++ crawling.py
@@ -11,7 +11,7 @@ URL_DICT = {
     '홈페이지': 'http://www.hanbat.ac.kr/_prog/gboard/board.php?code=news&GotoPage=1',
     '학사공지': 'http://www.hanbat.ac.kr/_prog/gboard/board.php?code=bachelor&GotoPage=1',
     '컴공': 'http://newclass.hanbat.ac.kr/ctnt/computer/board.php?mode=list&tbl_cd=computer_notice',
-    'SA사업단': 'http://newclass.hanbat.ac.kr/ctnt/computer/board.php?mode=list&tbl_cd=biz_notice',
+    # 'SA사업단': 'http://newclass.hanbat.ac.kr/ctnt/computer/board.php?mode=list&tbl_cd=biz_notice',
     'IT융합': 'http://www.ithanbat.kr/0201',
 }
 
